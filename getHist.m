84bbaf289e168c86f0c14670ac14815059186c70
shulincome <zhaozqa@qq.com>
2021-01-07 15:18:26 +0800

    1.7

--- getHist.m
+++ getHist.m
@@ -2,7 +2,7 @@ clc;
 clear;
 close all;
 %获取平滑的直方图
-src = imread("G:\学位论文GIT\article\处理后图像\310nm\纯海水\P54110000_imCp1.png");
+src = imread("G:\学位论文GIT\article\处理后图像\330nm\400um\P00290000_imCp1.png");
 imhist(src)
 [h,x] = imhist(src);
 h = smooth(h,7);
---
+++ getImg.m
@@ -0,0 +1,10 @@
+clc;
+clear;
+
+%读取图像
+img_300nm = imread('处理后图像\300nm\400um\P47420000_imCp1.png');
+img_310nm = imread('处理后图像\310nm\400um\P55350000_imCp1.png');
+img_330nm = imread('处理后图像\330nm\400um\P00260000_imCp1.png');
+
+%整体显示
+
